1c6c2095e7d267b9e7dbe7eb1f8d2c62456528ad
Markus Murschitz <markus.murschitz@ait.ac.at>
2023-06-15 11:16:45 +0200

    equal axis plotting for matplotlib 3.7.1

--- cylinder_fitting/visualize.py
+++ cylinder_fitting/visualize.py
@@ -1,16 +1,17 @@
-import numpy as np
 import matplotlib
-matplotlib.use('TkAgg') 
+import numpy as np
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
-from .geometry import rotation_matrix_from_axis_and_angle 
 from . import fitting
+from .geometry import rotation_matrix_from_axis_and_angle
 
 
 def show_G_distribution(data):
-    '''Show the distribution of the G function.'''
-    Xs, t = fitting.preprocess_data(data)  
+    """Show the distribution of the G function."""
+    Xs, t = fitting.preprocess_data(data)
 
     Theta, Phi = np.meshgrid(np.linspace(0, np.pi, 50), np.linspace(0, 2 * np.pi, 50))
     G = []
@@ -21,31 +22,62 @@ def show_G_distribution(data):
             w = fitting.direction(Theta[i][j], Phi[i][j])
             G[-1].append(fitting.G(w, Xs))
 
-    plt.imshow(G, extent=[0, np.pi, 0, 2 * np.pi], origin='lower')
+    plt.imshow(G, extent=[0, np.pi, 0, 2 * np.pi], origin="lower")
     plt.show()
 
-def show_fit(w_fit, C_fit, r_fit, Xs):
-    '''Plot the fitting given the fitted axis direction, the fitted
-    center, the fitted radius and the data points.
-    '''
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    
+# https://stackoverflow.com/questions/13685386/how-to-set-the-equal-aspect-ratio-for-all-axes-x-y-z
+# Functions from @Mateen Ulhaq and @karlo
+def set_axes_equal(ax: plt.Axes):
+    """Set 3D plot axes to equal scale.
+
+    Make axes of 3D plot have equal scale so that spheres appear as
+    spheres and cubes as cubes.  Required since `ax.axis('equal')`
+    and `ax.set_aspect('equal')` don't work on 3D.
+    """
+    limits = np.array(
+        [
+            ax.get_xlim3d(),
+            ax.get_ylim3d(),
+            ax.get_zlim3d(),
+        ]
+    )
+    origin = np.mean(limits, axis=1)
+    radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+    _set_axes_radius(ax, origin, radius)
+
+
+def _set_axes_radius(ax, origin, radius):
+    x, y, z = origin
+    ax.set_xlim3d([x - radius, x + radius])
+    ax.set_ylim3d([y - radius, y + radius])
+    ax.set_zlim3d([z - radius, z + radius])
+
+
+def show_fit(w_fit, C_fit, r_fit, Xs):
+    """Plot the fitting given the fitted axis direction, the fitted
+    center, the fitted radius and the data points.
+    """
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection="3d")
+
     # Plot the data points
-    
+
     ax.scatter([X[0] for X in Xs], [X[1] for X in Xs], [X[2] for X in Xs])
-   
+
     # Get the transformation matrix
 
     theta = np.arccos(np.dot(w_fit, np.array([0, 0, 1])))
     phi = np.arctan2(w_fit[1], w_fit[0])
 
-    M = np.dot(rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
-               rotation_matrix_from_axis_and_angle(np.array([0, 1, 0]), theta))
+    M = np.dot(
+        rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
+        rotation_matrix_from_axis_and_angle(np.array([0, 1, 0]), theta),
+    )
 
     # Plot the cylinder surface
-   
+
     delta = np.linspace(-np.pi, np.pi, 20)
     z = np.linspace(-10, 10, 20)
 
@@ -65,8 +97,18 @@ def show_fit(w_fit, C_fit, r_fit, Xs):
 
     # Plot the center and direction
 
-    ax.quiver(C_fit[0], C_fit[1], C_fit[2], 
-            r_fit * w_fit[0], r_fit * w_fit[1], r_fit * w_fit[2], color='red')
+    ax.quiver(
+        C_fit[0],
+        C_fit[1],
+        C_fit[2],
+        r_fit * w_fit[0],
+        r_fit * w_fit[1],
+        r_fit * w_fit[2],
+        color="red",
+    )
 
+    # Set the axes equal
+    set_axes_equal(ax)
+    ax.set_box_aspect([1, 1, 1])
 
     plt.show()
